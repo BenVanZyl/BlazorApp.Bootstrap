81638bbc35810b21493e772ece99a97ce93566fc
Ben van Zyl <Benvz@outlook.com>
2025-05-05 00:12:47 +0800

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # BlazorApp.Bootstrap
-Experimenting creating good apps in an easy, standarised way, using Blazor and Bootstrap
+Experimenting creating good apps in an easy and standarised way, using Blazor and Bootstrap
+- .NET 9
+- Blazor Server project
+- Bootstrap
---
+++ src/BlazorApp.Bootstrap.Data/Dtos/ContactDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Bootstrap.Data.Dtos
+{
+    public class ContactDto
+    {
+        [Required]
+        public string SenderName { get; set; } = string.Empty;
+
+        [Required]
+        public string Message { get; set; } = string.Empty;
+    }
+}
